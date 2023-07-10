50d9d4770be9594e626b9c7fd836004c7140743d
Krisna31 <krisnaaaputraaa@gmail.com>
2023-07-10 17:27:13 +0700

    init android for github android krisna31

---
+++ app/src/main/java/com/example/krisna31/github_api_consumer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.krisna31.github_api_consumer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
