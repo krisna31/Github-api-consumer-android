407fe2aac0fd910ceed65a99bbb4d83b1486fefe
Krisna31 <krisnaaaputraaa@gmail.com>
2023-07-10 18:54:13 +0700

    its works!

--- app/src/main/java/com/example/krisna31/github_api_consumer/data/retrofit/ApiService.kt
+++ app/src/main/java/com/example/krisna31/github_api_consumer/data/retrofit/ApiService.kt
@@ -3,11 +3,11 @@ package com.example.krisna31.github_api_consumer.data.retrofit
 import com.example.krisna31.github_api_consumer.data.response.SearchUser
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
-    @GET("search/users?q={username}")
+    @GET("search/users")
     fun getUser(
-        @Path("username") username: String
+        @Query("q") username: String
     ): Call<SearchUser>
 }
