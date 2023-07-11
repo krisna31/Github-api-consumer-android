590a92a92032c5b2ce4a52be4071d7879d4940e6
Krisna31 <krisnaaaputraaa@gmail.com>
2023-07-11 14:35:09 +0700

    add detail user not done

--- app/src/main/java/com/example/krisna31/github_api_consumer/data/retrofit/ApiService.kt
+++ app/src/main/java/com/example/krisna31/github_api_consumer/data/retrofit/ApiService.kt
@@ -1,8 +1,10 @@
 package com.example.krisna31.github_api_consumer.data.retrofit
 
+import com.example.krisna31.github_api_consumer.data.response.DetailUserResponse
 import com.example.krisna31.github_api_consumer.data.response.SearchUser
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
@@ -10,4 +12,7 @@ interface ApiService {
     fun getUser(
         @Query("q") username: String
     ): Call<SearchUser>
+
+    @GET("users/{username}")
+    fun getDetailUser(@Path("username") username: String): Call<DetailUserResponse>
 }
---
+++ app/src/main/java/com/example/krisna31/github_api_consumer/ui/DetailUserActivity.kt
@@ -0,0 +1,12 @@
+package com.example.krisna31.github_api_consumer.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.krisna31.github_api_consumer.R
+
+class DetailUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_user)
+    }
+}
